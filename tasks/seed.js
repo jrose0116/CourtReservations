826969f13aa0f2d99f454c4b355e9bcd5e84afea
jrose0116 <jrose0116@gmail.com>
2023-03-09 01:20:03 -0500

    Add TODOs to seed file

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,1 +1,14 @@
 // TODO: Seed file for testing purposes
+
+// TODO: Open Db Connection
+// !DUMP DB : (careful)
+
+// TODO: Seed Users
+
+//TODO Verify User
+
+// TODO: Seed Groups
+
+// TODO: Seed Friends
+
+// TODO: Close Connection

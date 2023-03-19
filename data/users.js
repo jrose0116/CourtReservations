7f2acc18a9094b7a6dd254fa52a9d4f39a1f3acf
jrose0116 <jrose0116@gmail.com>
2023-03-19 03:13:35 -0400

    Change validation/helper functions

--- data/users.js
+++ data/users.js
@@ -1,40 +1,23 @@
 import { users } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
-
-const validId = (id, errorPrefix) => {
-  if (!id) throw errorPrefix + " Id not provided";
-  if (typeof id !== "string") throw errorPrefix + " Id must be a string";
-  if (id.trim().length === 0)
-    throw errorPrefix + " Id must be a non-empty string that is not whitespace";
-  id = id.trim();
-  if (!ObjectId.isValid(id)) throw errorPrefix + " Id is invalid";
-  return id;
-};
-
-const findOne = async (obj, errorPrefix) => {
-  if (!obj) throw errorPrefix + " Object does not exist";
-
-  const usersCollection = await users();
-  const user = await usersCollection.findOne(obj);
-
-  if (user === null) throw errorPrefix + " No user found";
-
-  user._id = user._id.toString();
-  return user;
-};
+import { validId, validStr, validStrArr, validNumber } from "../validation.js";
 
 const exportedMethods = {
   async getUserById(id) {
     try {
-      id = validId(id, "Error (data/users.js :: getUserById(id)):");
+      id = validId(id, "userId");
     } catch (e) {
-      throw e;
+      throw "Error (data/users.js :: getUserById(id)):" + e;
     }
 
-    return await findOne(
-      { _id: new ObjectId(id) },
-      "Error (data/users.js :: getUserById(id)):"
-    );
+    const usersCollection = await users();
+    const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+
+    if (user === null)
+      throw "Error (data/users.js :: getUserById(id)): No user found";
+
+    user._id = user._id.toString();
+    return user;
   },
 };
 

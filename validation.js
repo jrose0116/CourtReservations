16d9d142d4b55ccffe51696fcf77c472220dc447
Isabella Stone <istone@stevens.edu>
2023-04-06 13:37:22 -0400

    typo

--- validation.js
+++ validation.js
@@ -105,7 +105,7 @@ const validState = (state) => {
 
 	let states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 
 				'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
-	if (!state.includes(state)) {
+	if (!states.includes(state)) {
 		throw `Error: State must be valid state.`
 	}
 	return state;

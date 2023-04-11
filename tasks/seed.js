3c11413095355e088b2c1f54df60f1e9dc593242
Isabella Stone <Isabella Stone>
2023-04-10 21:17:46 -0400

    fix headers

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,9 +1,17 @@
 // TODO: Seed file for testing purposes
+import {createCourt} from '../data/courts.js';
+import {dbConnection, closeConnection} from '../config/mongoConnection.js';
 
 // TODO: Open Db Connection
+const db = await dbConnection();
 // !DUMP DB : (careful)
+await db.dropDatabase();
 
 // TODO: Seed Users
+
+// TODO: Seed Courts
+let court = await createCourt("Court1");
+console.log(court);
 
 //TODO Verify User
 
@@ -12,3 +20,11 @@
 // TODO: Seed Friends
 
 // TODO: Close Connection
+await closeConnection();
+console.log('\nDone!');
+
+
+
+
+
+

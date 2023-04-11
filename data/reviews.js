0db8c4330a2352adfbd096c999801742712db7ca
Isabella Stone <Isabella Stone>
2023-04-10 21:27:35 -0400

    fix reservations

--- data/reviews.js
+++ data/reviews.js
@@ -1,171 +1,171 @@
 import { users } from "../config/mongoCollections.js";
 import { validId, validStr, validStrArr, validNumber } from "../validation.js";
 
-const exportedMethods = {
-	async createReview(revieweeId, reviewerId, rating, comment) {
-		try {
-			revieweeId = validId(revieweeId);
-			reviewerId = validId(reviewerId);
-		} catch (e) {
-			throw (
-				"Error (data/reviews.js :: createReview(revieweeId, reviewerId, rating, comment)):" +
-				e
-			);
-		}
-
-		const usersCollection = await users();
-		const reviewee = await usersCollection.findOne({
-			_id: new ObjectId(revieweeid),
-		});
-
-		if (reviewee === null)
-			throw "Error (data/reviews.js :: createReview(revieweeId, reviewerId, rating, comment)): Reviewed user not found";
-
-		reviews = reviewee.reviews;
-		for (let i of reviews) {
-			if (i.reviewee_id.toString() == revieweeId) {
-				throw "Error (data/reviews.js :: createReview(revieweeId, reviewerId, rating, comment)): User already contains review from user (delete review to create a new one)";
-			}
-		}
-
-		const reviewer = await usersCollection.findOne({
-			_id: new ObjectId(reviewerId),
-		});
-
-		if (reviewee === null)
-			throw "Error (data/reviews.js :: createReview(revieweeId, reviewerId, rating, comment)): Review poster not found";
-
-		try {
-			rating = validNumber(rating, "Rating", false, 1, 5);
-			comment = validStr(comment, "Comment");
-		} catch (e) {
-			throw (
-				"Error (data/reviews.js :: createReview(revieweeId, reviewerId, rating, comment)):" +
-				e
-			);
-		}
-
-		date = new Date();
-		const year = date.getFullYear();
-		const month = date.getMonth();
-		const day = date.getDate();
-
-		month = ("" + month).length == 1 ? "0" + month : month;
-		day = ("" + day).length == 1 ? "0" + day : day;
-
-		dateString = `${month}/${day}/${year}`;
-
-		let reviewObject = {
-			_id: new ObjectId(),
-			reviewerUsername: reviewee._id.toString(),
-			revieweeUsername: reviewer._id.toString(),
-			reviewer_id: reviewerId,
-			reviewee_id: revieweeId,
-			rating: rating,
-			comment: comment,
-			date: dateString,
-		};
-
-		reviews.push(reviewObject);
-
-		const updatedInfo = await usersCollection.findOneAndUpdate(
-			{ _id: new ObjectId(revieweeId) },
-			{ $set: { reviews: reviews } },
-			{ returnDocument: "after" }
+const createReview = async (revieweeId, reviewerId, rating, comment) => {
+	try {
+		revieweeId = validId(revieweeId);
+		reviewerId = validId(reviewerId);
+	} catch (e) {
+		throw (
+			"Error (data/reviews.js :: createReview(revieweeId, reviewerId, rating, comment)):" +
+			e
 		);
+	}
 
-		if (updatedInfo.lastErrorObject.n === 0)
-			throw "Error (data/reviews.js :: createReview(revieweeId, reviewerId, rating, comment)): Could not update user";
+	const usersCollection = await users();
+	const reviewee = await usersCollection.findOne({
+		_id: new ObjectId(revieweeid),
+	});
 
-		reviewObject._id = reviewObject._id.toString();
-		await updateOverallRating(revieweeId);
+	if (reviewee === null)
+		throw "Error (data/reviews.js :: createReview(revieweeId, reviewerId, rating, comment)): Reviewed user not found";
 
-		return reviewObject;
-	},
-	async deleteReview(reviewId) {
-		try {
-			reviewId = validId(reviewId);
-		} catch (e) {
-			throw "Error (data/reviews.js :: deleteReview(reviewId)): " + e;
+	reviews = reviewee.reviews;
+	for (let i of reviews) {
+		if (i.reviewee_id.toString() == revieweeId) {
+			throw "Error (data/reviews.js :: createReview(revieweeId, reviewerId, rating, comment)): User already contains review from user (delete review to create a new one)";
 		}
+	}
 
-		const usersCollection = await users();
-		const user = await usersCollection.findOne({
-			reviews: { $elemMatch: { _id: new ObjectId(reviewId) } },
-		});
+	const reviewer = await usersCollection.findOne({
+		_id: new ObjectId(reviewerId),
+	});
 
-		if (user == null)
-			throw "Error (data/reviews.js :: deleteReview(reviewId)): Review not found";
+	if (reviewee === null)
+		throw "Error (data/reviews.js :: createReview(revieweeId, reviewerId, rating, comment)): Review poster not found";
 
-		let found = false;
-		let reviews = user.reviews;
-		for (let i in reviews) {
-			if (reviews[i]._id.toString() == reviewId) {
-				reviews[i]._id = reviews[i]._id.toString();
-				reviews.splice(i, 1);
-				found = true;
-			}
-		}
-
-		if (!found)
-			throw "Error (data/reviews.js :: deleteReview(reviewId)): Review not found";
-
-		const updatedInfo = await usersCollection.findOneAndUpdate(
-			{ _id: user._id },
-			{ $set: { reviews: reviews } },
-			{ returnDocument: "after" }
+	try {
+		rating = validNumber(rating, "Rating", false, 1, 5);
+		comment = validStr(comment, "Comment");
+	} catch (e) {
+		throw (
+			"Error (data/reviews.js :: createReview(revieweeId, reviewerId, rating, comment)):" +
+			e
 		);
+	}
 
-		if (updatedInfo.lastErrorObject.n === 0)
-			throw "Error (data/reviews.js :: deleteReview(reviewId)): Could not update user";
+	date = new Date();
+	const year = date.getFullYear();
+	const month = date.getMonth();
+	const day = date.getDate();
 
-		await updateOverallRating(user._id.toString());
+	month = ("" + month).length == 1 ? "0" + month : month;
+	day = ("" + day).length == 1 ? "0" + day : day;
 
-		updatedInfo.value._id = updatedInfo.value._id.toString();
-		for (let i of updatedInfo.value.reviews) {
-			i._id = i._id.toString();
-		}
+	dateString = `${month}/${day}/${year}`;
 
-		return updatedInfo.value;
-	},
-	async updateOverallRating(userId) {
-		try {
-			userId = validId(userId);
-		} catch (e) {
-			throw "Error (data/reviews.js :: updateOverallRating(userId)): " + e;
-		}
+	let reviewObject = {
+		_id: new ObjectId(),
+		reviewerUsername: reviewee._id.toString(),
+		revieweeUsername: reviewer._id.toString(),
+		reviewer_id: reviewerId,
+		reviewee_id: revieweeId,
+		rating: rating,
+		comment: comment,
+		date: dateString,
+	};
 
-		const usersCollection = await users();
-		const reviewee = await usersCollection.findOne({
-			_id: new ObjectId(userId),
-		});
+	reviews.push(reviewObject);
 
-		if (reviewee === null)
-			throw "Error (data/reviews.js :: updateOverallRating(userId)): User not found";
+	const updatedInfo = await usersCollection.findOneAndUpdate(
+		{ _id: new ObjectId(revieweeId) },
+		{ $set: { reviews: reviews } },
+		{ returnDocument: "after" }
+	);
 
-		reviews = reviewee.reviews;
-		let res = 0;
-		for (let i of reviews) {
-			res += i.rating;
-		}
-		res = reviews.length == 0 ? 0 : res / reviews.length;
+	if (updatedInfo.lastErrorObject.n === 0)
+		throw "Error (data/reviews.js :: createReview(revieweeId, reviewerId, rating, comment)): Could not update user";
 
-		const updatedInfo = await usersCollection.findOneAndUpdate(
-			{ _id: new ObjectId(userId) },
-			{ $set: { overallRating: res } },
-			{ returnDocument: "after" }
-		);
+	reviewObject._id = reviewObject._id.toString();
+	await updateOverallRating(revieweeId);
 
-		if (updatedInfo.lastErrorObject.n === 0)
-			throw "Error (data/reviews.js :: updateOverallRating(userId)): Could not update user";
-
-		updatedInfo.value._id = updatedInfo.value._id.toString();
-		for (let i of updatedInfo.value.reviews) {
-			i._id = i._id.toString();
-		}
-
-		return updatedInfo.value;
-	},
+	return reviewObject;
 };
 
-export default exportedMethods;
+const deleteReview = async (reviewId) => {
+	try {
+		reviewId = validId(reviewId);
+	} catch (e) {
+		throw "Error (data/reviews.js :: deleteReview(reviewId)): " + e;
+	}
+
+	const usersCollection = await users();
+	const user = await usersCollection.findOne({
+		reviews: { $elemMatch: { _id: new ObjectId(reviewId) } },
+	});
+
+	if (user == null)
+		throw "Error (data/reviews.js :: deleteReview(reviewId)): Review not found";
+
+	let found = false;
+	let reviews = user.reviews;
+	for (let i in reviews) {
+		if (reviews[i]._id.toString() == reviewId) {
+			reviews[i]._id = reviews[i]._id.toString();
+			reviews.splice(i, 1);
+			found = true;
+		}
+	}
+
+	if (!found)
+		throw "Error (data/reviews.js :: deleteReview(reviewId)): Review not found";
+
+	const updatedInfo = await usersCollection.findOneAndUpdate(
+		{ _id: user._id },
+		{ $set: { reviews: reviews } },
+		{ returnDocument: "after" }
+	);
+
+	if (updatedInfo.lastErrorObject.n === 0)
+		throw "Error (data/reviews.js :: deleteReview(reviewId)): Could not update user";
+
+	await updateOverallRating(user._id.toString());
+
+	updatedInfo.value._id = updatedInfo.value._id.toString();
+	for (let i of updatedInfo.value.reviews) {
+		i._id = i._id.toString();
+	}
+
+	return updatedInfo.value;
+};
+
+const updateOverallRating = async(userId) => {
+	try {
+		userId = validId(userId);
+	} catch (e) {
+		throw "Error (data/reviews.js :: updateOverallRating(userId)): " + e;
+	}
+
+	const usersCollection = await users();
+	const reviewee = await usersCollection.findOne({
+		_id: new ObjectId(userId),
+	});
+
+	if (reviewee === null)
+		throw "Error (data/reviews.js :: updateOverallRating(userId)): User not found";
+
+	reviews = reviewee.reviews;
+	let res = 0;
+	for (let i of reviews) {
+		res += i.rating;
+	}
+	res = reviews.length == 0 ? 0 : res / reviews.length;
+
+	const updatedInfo = await usersCollection.findOneAndUpdate(
+		{ _id: new ObjectId(userId) },
+		{ $set: { overallRating: res } },
+		{ returnDocument: "after" }
+	);
+
+	if (updatedInfo.lastErrorObject.n === 0)
+		throw "Error (data/reviews.js :: updateOverallRating(userId)): Could not update user";
+
+	updatedInfo.value._id = updatedInfo.value._id.toString();
+	for (let i of updatedInfo.value.reviews) {
+		i._id = i._id.toString();
+	}
+
+	return updatedInfo.value;
+};
+
+export {createReview, deleteReview, updateOverallRating};

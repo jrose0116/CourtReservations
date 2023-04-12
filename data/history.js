53844ead033309fe3a500aa2e2d419a1d9eb15ae
jrose0116 <jrose0116@gmail.com>
2023-04-12 00:10:44 -0400

    History Data Functions

--- data/history.js
+++ data/history.js
@@ -1,17 +1,159 @@
-import { users } from "../config/mongoCollections.js";
-import { validId, validStr, validStrArr, validNumber } from "../validation.js";
+import { users, courts } from "../config/mongoCollections.js";
+import {
+  validId,
+  validStr,
+  validStrArr,
+  validNumber,
+  validTime,
+  validDate,
+} from "../validation.js";
 
 const appendToHistory = async (userId, courtId, date, startTime, endTime) => {
-  //
-};
-const deleteHistoryItem = async (historyItemId) => {
-  //
-};
-const getHistory = async (userId) => {
-  //
-};
-const getHistoryItem = async (historyItem) => {
-  //
+  try {
+    userId = validId(userId);
+    courtId = validId(courtId);
+    date = validDate(date);
+    startTime = validTime(startTime, false);
+    endTime = validTime(endTime, true);
+  } catch (e) {
+    throw (
+      "Error (data/history.js :: appendToHistory(userId, courtId, date, startTime, endTime)):" +
+      e
+    );
+  }
+
+  const usersCollection = await users();
+  const user = await usersCollection.findOne({
+    _id: new ObjectId(userId),
+  });
+
+  if (user === null)
+    throw "Error (data/history.js :: appendToHistory(userId, courtId, date, startTime, endTime)): User not found";
+
+  const courtsCollection = await courts();
+  const court = await courtsCollection.findOne({
+    _id: new ObjectId(courtId),
+  });
+
+  if (court === null)
+    throw "Error (data/history.js :: appendToHistory(userId, courtId, date, startTime, endTime)): Court not found";
+
+  let history = user.history;
+  let historyEntry = {
+    _id: new ObjectId(),
+    court_id: courtId,
+    date,
+    startTime,
+    endTime,
+  };
+  history.push(historyEntry);
+
+  const updatedInfo = await usersCollection.findOneAndUpdate(
+    { _id: new ObjectId(userId) },
+    { $set: { history: history } },
+    { returnDocument: "after" }
+  );
+
+  if (updatedInfo.lastErrorObject.n === 0)
+    throw "Error (data/history.js :: appendToHistory(userId, courtId, date, startTime, endTime)): Could not update user";
+
+  historyEntry._id = historyEntry._id.toString();
+
+  return historyEntry;
 };
 
-export {appendToHistory, deleteHistoryItem, getHistory, getHistoryItem};
+const deleteHistoryItem = async (historyItemId) => {
+  try {
+    historyItemId = validId(historyItemId);
+  } catch (e) {
+    throw "Error (data/history.js :: deleteHistoryItem(historyItemId): " + e;
+  }
+
+  const usersCollection = await users();
+  const user = await usersCollection.findOne({
+    history: { $elemMatch: { _id: new ObjectId(historyItemId) } },
+  });
+
+  if (user == null)
+    throw "Error (data/history.js :: deleteHistoryItem(historyItemId): History Item not found";
+
+  let found = false;
+  let history = user.history;
+  for (let i in history) {
+    if (history[i]._id.toString() == historyItemId) {
+      history[i]._id = history[i]._id.toString();
+      reviews.splice(i, 1);
+      found = true;
+    }
+  }
+
+  if (!found)
+    throw "Error (data/history.js :: deleteHistoryItem(historyItemId): History Item not found";
+
+  const updatedInfo = await usersCollection.findOneAndUpdate(
+    { _id: user._id },
+    { $set: { history: history } },
+    { returnDocument: "after" }
+  );
+
+  if (updatedInfo.lastErrorObject.n === 0)
+    throw "Error (data/history.js :: deleteHistoryItem(historyItemId): Could not update user";
+
+  updatedInfo.value._id = updatedInfo.value._id.toString();
+  for (let i of updatedInfo.value.reviews) {
+    i._id = i._id.toString();
+  }
+  for (let i of updatedInfo.value.history) {
+    i._id = i._id.toString();
+  }
+
+  return updatedInfo.value;
+};
+const getHistory = async (userId) => {
+  try {
+    userId = validId(userId);
+  } catch (e) {
+    throw "Error (data/history.js :: getHistory(userId)):" + e;
+  }
+
+  const usersCollection = await users();
+  const user = await usersCollection.findOne({
+    _id: new ObjectId(userId),
+  });
+
+  if (user === null)
+    throw "Error (data/history.js :: getHistory(userId)): User not found";
+
+  for (let i of user.history) {
+    i._id = i._id.toString();
+  }
+
+  return user.history;
+};
+const getHistoryItem = async (historyItemId) => {
+  try {
+    historyItemId = validId(historyItemId);
+  } catch (e) {
+    throw "Error (data/history.js :: getHistoryItem(historyItemId): " + e;
+  }
+
+  const usersCollection = await users();
+  const user = await usersCollection.findOne({
+    history: { $elemMatch: { _id: new ObjectId(historyItemId) } },
+  });
+
+  if (user == null)
+    throw "Error (data/history.js :: getHistoryItem(historyItemId): History Item not found";
+
+  let history = user.history;
+  for (let i in history) {
+    if (history[i]._id.toString() == historyItemId) {
+      history[i]._id = history[i]._id.toString();
+      return history[i];
+    }
+  }
+
+  throw "Error (data/history.js :: getHistoryItem(historyItemId): History Item not found";
+};
+
+export { appendToHistory, deleteHistoryItem, getHistory, getHistoryItem };

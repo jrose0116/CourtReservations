2c26ef42bb0c261e03058b938b6b0792392b9633
Isabella Stone <ibellarose1@gmail.com>
2023-04-15 17:38:18 -0400

    work on court handlebars

--- routes/courts.js
+++ routes/courts.js
@@ -2,18 +2,19 @@ import { Router } from "express";
 const router = Router();
 import {createCourt, getCourtById, getAllCourts, getCourtsByName} from '../data/courts.js';
 
-router.route("/list/").get(async (req, res) => {
-  // return res.json({ route: "Available courts page" });
+router.route("/available/").get(async (req, res) => {
   let courtList = await getAllCourts();
   return res.render('../views/allCourts', {title: 'Available Courts', courts: courtList});
 });
 
 router.route("/:courtId/").get(async (req, res) => {
-  // return res.json({ courtId: req.params.courtId, implementMe: "<-" });
-  console.log(req.params.courtId);
   let thisCourt = await getCourtById(req.params.courtId);
-  console.log(thisCourt);
-  return res.render('../views/courtById', {title: '', court: thisCourt});
+  return res.render('../views/courtById', {title: thisCourt.name, court: thisCourt});
+});
+
+router.route("/:courtId/reserve").get(async (req, res) => {
+  let thisCourt = await getCourtById(req.params.courtId);
+  return res.render('../views/makeReservation', {title: 'Make Reservation', court: thisCourt, id: thisCourt._id});
 });
 
 router.route("/recommend/").get((req, res) => {
@@ -28,14 +29,6 @@ router
   .post((req, res) => {
     return res.json({ create: "post" });
   });
-
-// router.route("/:courtId/reserve").get((req, res) => {
-//   return res.json({
-//     courtId: req.params.courtId,
-//     reserve: "this",
-//     implementMe: "<-",
-//   });
-// });
 
 // router.route("/:courtId/cancel").get((req, res) => {
 //   return res.json({
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const constructor = (app) => {
   app.use("/", utilRoutes); // TODO: Handles settings, search, history, recommendations (Without actually using the '/' route)
 
   app.use("/user", userRoutes); // TODO: Handle own profile, other user profiles, reviews, etc.
-  app.use("/court", courtRoutes); // TODO: Handle courts, court listings (book, cancel, rate), etc.
+  app.use("/courts", courtRoutes); // TODO: Handle courts, court listings (book, cancel, rate), etc.
   app.use("/group", groupRoutes); // TODO: Handle groups
 
   // app.use("/admin", adminRoutes); // TODO: Handle potential admin dashboard. Ability to verify profiles and remove listings, groups, etc.

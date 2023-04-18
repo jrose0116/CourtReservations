9b9fd54f2d2454acf6e3b74babacbcf4437e178f
jrose0116 <jrose0116@gmail.com>
2023-04-18 03:13:30 -0400

    toString() review/history ids + Prettier

--- data/users.js
+++ data/users.js
@@ -1,108 +1,132 @@
 import { users } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
-import { validId, validStr, validNumber, validState, validZip, validTime, validTimeInRange, validEmail, validExpLevel, validImageUrl} from "../validation.js";
+import {
+  validId,
+  validStr,
+  validNumber,
+  validState,
+  validZip,
+  validTime,
+  validTimeInRange,
+  validEmail,
+  validExpLevel,
+  validImageUrl,
+} from "../validation.js";
 
 //HASH PASSWORD
-const createUser = async(
-    firstName,
-    lastName,
-    username,
-    password,
-    age,
-    city,
-    state,
-    zip,
-    email,
-    experience_level,
-    owner,
-    image,
-  ) => {
-    if (!firstName || !lastName || !username || !password || !age || !city || !state || !zip || !email || !experience_level) {
-      throw "Error: All inputs must be provided";
+const createUser = async (
+  firstName,
+  lastName,
+  username,
+  password,
+  age,
+  city,
+  state,
+  zip,
+  email,
+  experience_level,
+  owner,
+  image
+) => {
+  if (
+    !firstName ||
+    !lastName ||
+    !username ||
+    !password ||
+    !age ||
+    !city ||
+    !state ||
+    !zip ||
+    !email ||
+    !experience_level
+  ) {
+    throw "Error: All inputs must be provided";
   }
   if (owner === null) {
-    throw "Error: owner must be provided"
+    throw "Error: owner must be provided";
   }
   if (!image) {
-    image = '/public/images/No_Image_Available.jpg';
+    image = "/public/images/No_Image_Available.jpg";
   } else {
     image = validImageUrl(image);
   }
-    try {
-      firstName = validStr(firstName, "First name");
-      lastName = validStr(lastName, "Last name");
-      username = validStr(username, "Username");
-      city = validStr(city, "City");
-    } catch (e) {
-      throw e;
-    }
-    try {
-      age = validNumber(age, "Age");
-    } catch (e) {
-      throw e;
-    }
-    // age must be above 13
-    if (age < 13) {
-      throw "Error: user must be 13 or older to join";
-    }
-    // CHECK CITY SOMEHOW
-    try {
-      state = validState(state);
-    } catch (e) {
-      throw e;
-    }
-    try {
-      zip = validZip(zip);
-    } catch (e) {
-      throw (e);
-    }
-     try {
-      email = validEmail(email);
-    } catch (e) {
-      throw (e);
-    }
-     try {
-      experience_level = validExpLevel(experience_level);
-    } catch (e) {
-      throw (e);
+  try {
+    firstName = validStr(firstName, "First name");
+    lastName = validStr(lastName, "Last name");
+    username = validStr(username, "Username");
+    city = validStr(city, "City");
+  } catch (e) {
+    throw e;
+  }
+  try {
+    age = validNumber(age, "Age");
+  } catch (e) {
+    throw e;
+  }
+  // age must be above 13
+  if (age < 13) {
+    throw "Error: user must be 13 or older to join";
+  }
+  // CHECK CITY SOMEHOW
+  try {
+    state = validState(state);
+  } catch (e) {
+    throw e;
+  }
+  try {
+    zip = validZip(zip);
+  } catch (e) {
+    throw e;
+  }
+  try {
+    email = validEmail(email);
+  } catch (e) {
+    throw e;
+  }
+  try {
+    experience_level = validExpLevel(experience_level);
+  } catch (e) {
+    throw e;
   }
   if (typeof owner !== "boolean") {
     throw "Error: owner must be of type boolean";
   }
-    let addUser = {
-      firstName: firstName,
-      lastName: lastName,
-      username: username.toLowerCase(),
-      password: password,
-      age: age,
-      city: city,
-      state: state,
-      zip: zip,
-      email: email.toLowerCase(),
-      image: image,
-      experience_level: experience_level,
-      owner: owner,
-      reviews: [],
-      history: [],
-      overallRating: 0
-    }
-    const usersCollection = await users();
-    // check if username already exists
-    const checkUsername = await usersCollection.findOne({ username: new RegExp("^" + username, "i") });
-    if (checkUsername !== null) {
-      throw "Error: another user has this username."
-    }
-    const insertInfo = await usersCollection.insertOne(addUser);
-    if (!insertInfo.acknowledged || !insertInfo.insertedId)
-      throw 'Could not add band';
+  let addUser = {
+    firstName: firstName,
+    lastName: lastName,
+    username: username.toLowerCase(),
+    password: password,
+    age: age,
+    city: city,
+    state: state,
+    zip: zip,
+    email: email.toLowerCase(),
+    image: image,
+    experience_level: experience_level,
+    owner: owner,
+    reviews: [],
+    history: [],
+    overallRating: 0,
+  };
+  const usersCollection = await users();
+  // check if username already exists
+  const checkUsername = await usersCollection.findOne({
+    username: new RegExp("^" + username, "i"),
+  });
+  if (checkUsername !== null) {
+    throw "Error: another user has this username.";
+  }
+  const insertInfo = await usersCollection.insertOne(addUser);
+  if (!insertInfo.acknowledged || !insertInfo.insertedId)
+    throw "Could not add band";
 
-    const newId = insertInfo.insertedId.toString();
+  const newId = insertInfo.insertedId.toString();
 
-    const user = await getUserById(newId);
-    return user;
+  const user = await getUserById(newId);
+  return user;
 };
 
-const getUserById = async(id) => {
+const getUserById = async (id) => {
   try {
     id = validId(id, "userId");
   } catch (e) {
@@ -116,148 +140,190 @@ const getUserById = async(id) => {
     throw "Error (data/users.js :: getUserById(id)): No user found";
 
   user._id = user._id.toString();
+  for (let i of user.reviews) {
+    i._id = i._id.toString();
+  }
+  for (let i of user.history) {
+    i._id = i._id.toString();
+  }
   return user;
 };
 
 // returns array of objects of all the people with that name
-const getUserByName = async(firstname, lastname) => {
-    try {
-      firstname = validStr(firstname);
-      lastname = validStr(lastname);
-    } catch (e) {
-      throw e;
-    }
-    const usersCollection = await users();
-    // case insensitive search
-    const user = await usersCollection.find({firstName: { $regex : new RegExp("^" + firstname, "i") }, lastName: { $regex : new RegExp("^" + lastname, "i") } }).toArray();
-    if (user.length === 0) throw 'No user with that name';
-    for (let i = 0; i < user.length; i++){
-      user[i]._id = user[i]._id.toString();
-    }
-    return user;
-};
-
-const getUserByUsername = async(username) => {
-    try {
-      username = validStr(username);
-    } catch (e) {
-      throw e;
-    }
-    const usersCollection = await users();
-    const user = await usersCollection.findOne({ username: { $regex: new RegExp("^" + username, "i") } });
-    if (user === null) throw 'No user with that username';
-    user._id = user._id.toString()
-    return user;
-};
-
-  // does not update password
-const updateUser = async(
-    id,
-    firstName,
-    lastName,
-    username,
-    age,
-    city,
-    state,
-    zip,
-    email,
-    experience_level,
-    owner,
-    image,
-
-  ) => {
-    if (!firstName || !lastName || !username || !age || !city || !state || !zip || !email || !experience_level) {
-      throw "Error: All inputs must be provided";
+const getUserByName = async (firstname, lastname) => {
+  try {
+    firstname = validStr(firstname);
+    lastname = validStr(lastname);
+  } catch (e) {
+    throw e;
   }
-   if (owner === null) {
-    throw "Error: owner must be provided"
+  const usersCollection = await users();
+  // case insensitive search
+  const user = await usersCollection
+    .find({
+      firstName: { $regex: new RegExp("^" + firstname, "i") },
+      lastName: { $regex: new RegExp("^" + lastname, "i") },
+    })
+    .toArray();
+  if (user.length === 0) throw "No user with that name";
+  for (let i = 0; i < user.length; i++) {
+    user[i]._id = user[i]._id.toString();
+    for (let i of user[i].reviews) {
+      i._id = i._id.toString();
+    }
+    for (let i of user[i].history) {
+      i._id = i._id.toString();
+    }
+  }
+  return user;
+};
+
+const getUserByUsername = async (username) => {
+  try {
+    username = validStr(username);
+  } catch (e) {
+    throw e;
+  }
+  const usersCollection = await users();
+  const user = await usersCollection.findOne({
+    username: { $regex: new RegExp("^" + username, "i") },
+  });
+  if (user === null) throw "No user with that username";
+  user._id = user._id.toString();
+  for (let i of user.reviews) {
+    i._id = i._id.toString();
+  }
+  for (let i of user.history) {
+    i._id = i._id.toString();
+  }
+  return user;
+};
+
+// does not update password
+const updateUser = async (
+  id,
+  firstName,
+  lastName,
+  username,
+  age,
+  city,
+  state,
+  zip,
+  email,
+  experience_level,
+  owner,
+  image
+) => {
+  if (
+    !firstName ||
+    !lastName ||
+    !username ||
+    !age ||
+    !city ||
+    !state ||
+    !zip ||
+    !email ||
+    !experience_level
+  ) {
+    throw "Error: All inputs must be provided";
+  }
+  if (owner === null) {
+    throw "Error: owner must be provided";
   }
   if (!image) {
-    image = '/public/images/No_Image_Available.jpg';
+    image = "/public/images/No_Image_Available.jpg";
   } else {
     image = validImageUrl(image);
   }
-      try {
-      firstName = validStr(firstName);
-      lastName = validStr(lastName);
-      username = validStr(username);
-      city = validStr(city);
-    } catch (e) {
-      throw e;
-    }
-    try {
-      age = validNumber(age);
-    } catch (e) {
-      throw e;
-    }
-    // age must be above 13
-    if (age < 13) {
-      throw "Error: user must be 13 or older to join";
-    }
-    // CHECK CITY SOMEHOW
-    try {
-      state = validState(state);
-    } catch (e) {
-      throw e;
-    }
-    try {
-      zip = validZip(zip);
-    } catch (e) {
-      throw (e);
-    }
-     try {
-      email = validEmail(email);
-    } catch (e) {
-      throw (e);
-    }
-     try {
-      experience_level = validExpLevel(experience_level);
-    } catch (e) {
-      throw (e);
-     }
-    try {
-      id = validId(id);
-    } catch (e) {
-      throw (e);
+  try {
+    firstName = validStr(firstName);
+    lastName = validStr(lastName);
+    username = validStr(username);
+    city = validStr(city);
+  } catch (e) {
+    throw e;
+  }
+  try {
+    age = validNumber(age);
+  } catch (e) {
+    throw e;
+  }
+  // age must be above 13
+  if (age < 13) {
+    throw "Error: user must be 13 or older to join";
+  }
+  // CHECK CITY SOMEHOW
+  try {
+    state = validState(state);
+  } catch (e) {
+    throw e;
+  }
+  try {
+    zip = validZip(zip);
+  } catch (e) {
+    throw e;
+  }
+  try {
+    email = validEmail(email);
+  } catch (e) {
+    throw e;
+  }
+  try {
+    experience_level = validExpLevel(experience_level);
+  } catch (e) {
+    throw e;
+  }
+  try {
+    id = validId(id);
+  } catch (e) {
+    throw e;
   }
   if (typeof owner !== "boolean") {
     throw "Error: owner must be of type boolean";
   }
-    let updatedUser = {
-      firstName: firstName,
-      lastName: lastName,
-      username: username,
-      age: age,
-      city: city,
-      state: state,
-      zip: zip,
-      email: email,
-      image: image,
-      experience_level: experience_level,
-      owner: owner
-    }
-    const usersCollection = await users();
-     const updateInfo = await usersCollection.findOneAndUpdate(
-       { _id: new ObjectId(id) },
-       { $set: updatedUser },
-       { returnDocument: 'after' }
-     );
-    if (updateInfo.lastErrorObject.n === 0) throw "Error: Update failed";
-    // check if username already exists
-     let finalUser = await updateInfo.value;
-     finalUser._id = finalUser._id.toString();
-     for (let i = 0; i < finalUser.reviews.length; i++){
-       finalUser.reviews[i]._id = finalUser.reviews[i]._id.toString();
-     }
-    for (let i = 0; i < finalUser.history.length; i++){
-       finalUser.history[i]._id = finalUser.history[i]._id.toString();
-    }
-    // check if username already exists
-    const checkUsername = await usersCollection.find({username: finalUser.username }).toArray()
-    if (checkUsername.length > 1) {
-      throw "Error: another user has this username."
-    }
-     return finalUser;
+  let updatedUser = {
+    firstName: firstName,
+    lastName: lastName,
+    username: username,
+    age: age,
+    city: city,
+    state: state,
+    zip: zip,
+    email: email,
+    image: image,
+    experience_level: experience_level,
+    owner: owner,
+  };
+  const usersCollection = await users();
+  const updateInfo = await usersCollection.findOneAndUpdate(
+    { _id: new ObjectId(id) },
+    { $set: updatedUser },
+    { returnDocument: "after" }
+  );
+  if (updateInfo.lastErrorObject.n === 0) throw "Error: Update failed";
+  // check if username already exists
+  let finalUser = await updateInfo.value;
+  finalUser._id = finalUser._id.toString();
+  for (let i = 0; i < finalUser.reviews.length; i++) {
+    finalUser.reviews[i]._id = finalUser.reviews[i]._id.toString();
+  }
+  for (let i = 0; i < finalUser.history.length; i++) {
+    finalUser.history[i]._id = finalUser.history[i]._id.toString();
+  }
+  // check if username already exists
+  const checkUsername = await usersCollection
+    .find({ username: finalUser.username })
+    .toArray();
+  if (checkUsername.length > 1) {
+    throw "Error: another user has this username.";
+  }
+  return finalUser;
 };
 
-export {createUser, getUserById, getUserByName, getUserByUsername, updateUser};
+export {
+  createUser,
+  getUserById,
+  getUserByName,
+  getUserByUsername,
+  updateUser,
+};

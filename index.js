6496a11f279dcaaa57a0f347de78dceb556e6102
jrose0116 <jrose0116@gmail.com>
2023-04-25 10:24:28 -0400

    Redirect middlewares + Prettier

--- index.js
+++ index.js
@@ -11,25 +11,25 @@ const __dirname = dirname(__filename);
 const staticDir = express.static(__dirname + "/public");
 
 app.use(
-	session({
-		name: "AuthCookie",
-		secret: "some secret string!",
-		resave: false,
-		saveUninitialized: false,
-	})
+  session({
+    name: "AuthCookie",
+    secret: "some secret string!",
+    resave: false,
+    saveUninitialized: false,
+  })
 );
 
 const rewriteUnsupportedBrowserMethods = (req, res, next) => {
-	// If the user posts to the server with a property called _method, rewrite the request's method
-	// To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
-	// rewritten in this middleware to a PUT route
-	if (req.body && req.body._method) {
-		req.method = req.body._method;
-		delete req.body._method;
-	}
+  // If the user posts to the server with a property called _method, rewrite the request's method
+  // To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
+  // rewritten in this middleware to a PUT route
+  if (req.body && req.body._method) {
+    req.method = req.body._method;
+    delete req.body._method;
+  }
 
-	// let the next middleware run:
-	next();
+  // let the next middleware run:
+  next();
 };
 
 app.use("/public", staticDir);
@@ -37,12 +37,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(rewriteUnsupportedBrowserMethods);
 
+app.use("/login", (req, res, next) => {
+  if (!req.session || !req.session.user) return next();
+  return res.redirect("/");
+});
+
+app.use("/register", (req, res, next) => {
+  if (!req.session || !req.session.user) return next();
+  return res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log(req.originalUrl);
+  if (
+    req.originalUrl.substring(0, 6) != "/login" &&
+    req.originalUrl.substring(0, 9) != "/register" &&
+    (!req.session || !req.session.user)
+  )
+    return res.redirect("/login");
+  next();
+});
+
 app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 configRoutes(app);
 
 app.listen(3000, () => {
-	console.log("We've now got a server!");
-	console.log("Your routes will be running on http://localhost:3000");
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
 });

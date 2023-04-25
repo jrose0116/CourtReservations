a1b452d6fbf75ff85653b28b5fd29cbf64e9be75
Jacob Rose <jrose0116@gmail.com>
2023-04-24 20:38:38 -0400

    Import session

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
-import express from 'express';
+import express from "express";
+import session from "express-session";
 const app = express();
-import configRoutes from './routes/index.js';
-import {fileURLToPath} from 'url';
-import {dirname} from 'path';
-import exphbs from 'express-handlebars';
+import configRoutes from "./routes/index.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import exphbs from "express-handlebars";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const staticDir = express.static(__dirname + '/public');
+const staticDir = express.static(__dirname + "/public");
 
-app.use(session({
-  name: 'AuthCookie',
-  secret: 'some secret string!',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+	session({
+		name: "AuthCookie",
+		secret: "some secret string!",
+		resave: false,
+		saveUninitialized: false,
+	})
+);
 
 const rewriteUnsupportedBrowserMethods = (req, res, next) => {
-  // If the user posts to the server with a property called _method, rewrite the request's method
-  // To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
-  // rewritten in this middleware to a PUT route
-  if (req.body && req.body._method) {
-    req.method = req.body._method;
-    delete req.body._method;
-  }
+	// If the user posts to the server with a property called _method, rewrite the request's method
+	// To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
+	// rewritten in this middleware to a PUT route
+	if (req.body && req.body._method) {
+		req.method = req.body._method;
+		delete req.body._method;
+	}
 
-  // let the next middleware run:
-  next();
+	// let the next middleware run:
+	next();
 };
 
-app.use('/public', staticDir);
+app.use("/public", staticDir);
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(rewriteUnsupportedBrowserMethods);
 
-app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 configRoutes(app);
 
 app.listen(3000, () => {
-  console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+	console.log("We've now got a server!");
+	console.log("Your routes will be running on http://localhost:3000");
 });

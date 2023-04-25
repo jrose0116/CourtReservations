97b9489e428034400a96066fd6339dc419042029
jrose0116 <jrose0116@gmail.com>
2023-04-25 17:14:00 -0400

    Home route / wildcard route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import userRoutes from "./users.js";
 import courtRoutes from "./courts.js";
 import groupRoutes from "./groups.js";
 import authRoutes from "./auth.js";
+import { getAllCourts } from "../data/courts.js";
 // import utilRoutes from "./utils.js";
 // import adminRoutes from "./admin.js";
 
@@ -16,9 +17,35 @@ const constructor = (app) => {
 
   // app.use("/admin", adminRoutes); // TODO: Handle potential admin dashboard. Ability to verify profiles and remove listings, groups, etc.
 
+  app.get("/", async (req, res) => {
+    if (!req.session || !req.session.user || !req.session.user.id) {
+      return res.render("homepage", { auth: false, title: "Home" });
+    }
+    let courtList;
+    try {
+      courtList = await getAllCourts();
+    } catch (e) {
+      return res.status(500).render("error", { error: e, status: 500 });
+    }
+    let ownedList = courtList.filter((court) => {
+      return court.ownerId == req.session.user.id;
+    });
+    courtList = courtList.filter((court) => {
+      return court.ownerId != req.session.user.id;
+    });
+    console.log(req.session.user);
+    res.render("homepage", {
+      auth: true,
+      title: "Home",
+      availableCourts: courtList,
+      ownedCourts: ownedList,
+      id: req.session.user.id,
+      owner: req.session.user.owner,
+    });
+  });
+
   app.use("*", (req, res) => {
-    if (!req.session || !req.session.user) return res.redirect("/login");
-    return res.redirect("/courts/available");
+    return res.redirect("/");
   });
 };
 

6fdeaffd3732954a0e92713c43c21d43dfe7adcf
jrose0116 <jrose0116@gmail.com>
2023-04-25 13:56:19 -0400

    History Error Handling + Render Error

--- routes/users.js
+++ routes/users.js
@@ -12,28 +12,6 @@ import { getHistory } from "../data/history.js";
 import { getCourtById } from "../data/courts.js";
 import { validId, validStr } from "../validation.js";
 
-router.route("/id/:userId").get(async (req, res) => {
-  let userId;
-  try {
-    userId = validId(req.params.userId);
-  } catch (e) {
-    return res.status(400).json({ error: e });
-  }
-  try {
-    let user = await getUserById(req.params.userId);
-    return res.render("profilePage", {
-      id: req.session.user.id,
-      title: req.params.username,
-      user: user,
-      reviews: user.reviews,
-      auth: true,
-    });
-  } catch (e) {
-    return res.status(404).json({ error: "User not found" });
-  }
-  //return res.json({ userId: req.params.userId, implementMe: "<-" });
-});
-
 router
   .route("/id/:userId/createReview")
   .get(async (req, res) => {
@@ -41,13 +19,17 @@ router
     try {
       userId = validId(req.params.userId);
     } catch (e) {
-      return res.status(400).json({ error: e });
+      return res
+        .status(400)
+        .render("error", { error: e, auth: true, status: 400 });
     }
     let user;
     try {
       user = await getUserById(userId);
     } catch (e) {
-      return res.status(404).json({ error: "User not found" });
+      return res
+        .status(404)
+        .render("error", { error: "User not found", auth: true, status: 404 });
     }
     return res.render("createReview", {
       title: req.params.username,
@@ -62,14 +44,18 @@ router
       reviewerId = validId(req.session.user.id);
       revieweeId = validId(req.params.userId);
     } catch (e) {
-      return res.status(400).json({ error: e });
+      return res
+        .status(400)
+        .render("error", { error: e, auth: true, status: 400 });
     }
     let reviewee, reviewer;
     try {
       reviewer = await getUserById(req.session.user.id);
       reviewee = await getUserById(req.params.userId);
     } catch (e) {
-      return res.status(404).json({ error: e });
+      return res
+        .status(404)
+        .render("error", { error: e, auth: true, status: 404 });
     }
     let reviewInfo = req.body;
     reviewInfo["reviewer_id"] = req.session.user.id;
@@ -106,11 +92,15 @@ router.route("/name/:username").get(async (req, res) => {
   try {
     usernameInput = validStr(usernameInput, "username");
   } catch (e) {
-    return res.status(400).json({ error: e });
+    return res
+      .status(400)
+      .render("error", { error: e, auth: true, status: 400 });
   }
   let user = await getUserByUsername(usernameInput);
   if (user === null) {
-    return res.status(404).json({ error: "User not found" });
+    return res
+      .status(404)
+      .render("error", { error: "User not found", auth: true, status: 404 });
   }
   return res.redirect("/id/" + user._id);
   //return res.json({ username: req.params.username, implementMe: "<-" });
@@ -121,11 +111,25 @@ router.route("/id/:userId/history").get(async (req, res) => {
   try {
     userId = validId(req.params.userId);
   } catch (e) {
-    return res.status(400).json({ error: e });
+    return res
+      .status(400)
+      .render("error", { error: e, auth: true, status: 400 });
   }
-  let courtHistory = await getHistory(userId);
+  let courtHistory;
+  try {
+    courtHistory = await getHistory(userId);
+  } catch (e) {
+    return res
+      .status(404)
+      .render("error", { error: e, status: 404, auth: true });
+  }
   for (let i = 0; i < courtHistory.length; i++) {
-    let court = await getCourtById(courtHistory[i].court_id);
+    let court;
+    try {
+      court = await getCourtById(courtHistory[i].court_id);
+    } catch (e) {
+      res.status(404).render("error", { error: e, status: 404, auth: true });
+    }
     courtHistory[i].court_name = court.name;
   }
   // console.log(courtHistory)
@@ -136,6 +140,32 @@ router.route("/id/:userId/history").get(async (req, res) => {
     courts: courtHistory,
     id: req.params.userId,
   });
+});
+
+router.route("/id/:userId").get(async (req, res) => {
+  let userId;
+  try {
+    userId = validId(req.params.userId);
+  } catch (e) {
+    return res
+      .status(400)
+      .render("error", { error: e, auth: true, status: 400 });
+  }
+  try {
+    let user = await getUserById(req.params.userId);
+    return res.render("profilePage", {
+      id: req.session.user.id,
+      title: req.params.username,
+      user: user,
+      reviews: user.reviews,
+      auth: true,
+    });
+  } catch (e) {
+    return res
+      .status(404)
+      .render("error", { error: "User not found", auth: true, status: 404 });
+  }
+  //return res.json({ userId: req.params.userId, implementMe: "<-" });
 });
 
 export default router;

b77bc2548ac71b5b40ef0b04b03f8864b8a087cb
jrose0116 <jrose0116@gmail.com>
2023-04-25 10:28:21 -0400

    Render variables + Prettier

--- routes/courts.js
+++ routes/courts.js
@@ -1,181 +1,192 @@
 import { Router } from "express";
 const router = Router();
 import {
-	createCourt,
-	getCourtById,
-	getAllCourts,
-	getCourtsByName,
+  createCourt,
+  getCourtById,
+  getAllCourts,
+  getCourtsByName,
 } from "../data/courts.js";
 import {
-	addToSchedule,
-	getSchedule,
-	removeFromSchedule,
-	clearSchedule,
-	getScheduleDate,
-	getBooking,
-	// checkBookingCapacity,
+  addToSchedule,
+  getSchedule,
+  removeFromSchedule,
+  clearSchedule,
+  getScheduleDate,
+  getBooking,
+  // checkBookingCapacity,
 } from "../data/schedule.js";
-import { validId, validStr, validStrArr, validNumber, validAddress, validState, validZip, validTime, validTimeInRange, validDate} from "../validation.js";
+import {
+  validId,
+  validStr,
+  validStrArr,
+  validNumber,
+  validAddress,
+  validState,
+  validZip,
+  validTime,
+  validTimeInRange,
+  validDate,
+} from "../validation.js";
 
-router.route("/available/").get(async (req, res) => {
-	let courtList = await getAllCourts();
-	let isAuth;
-	if (req.session.user) {
-		isAuth = true;
-	}
-	else {
-		isAuth = false;
-	}
-	return res.render("allCourts", {
-		title: "Available Courts",
-		courts: courtList,
-    auth: isAuth, 
-    id: req.session.user.id
-	});
+router.route("/available").get(async (req, res) => {
+  let courtList = await getAllCourts();
+  //   let isAuth;
+  //   if (req.session.user) {
+  //     isAuth = true;
+  //   } else {
+  //     isAuth = false;
+  //   }
+  return res.render("allCourts", {
+    title: "Courts",
+    courts: courtList,
+    auth: true,
+    id: req.session.user.id,
+  });
 });
 
 router
-	.route("/create/")
-	.get(async (req, res) => {
-		let isAuth;
-		if (req.session.user) {
-			isAuth = true;
-		}
-		else {
-			isAuth = false;
-		}
-		return res.render("createCourt", {auth: isAuth, id: req.session.user.id});
-	})
-	.post(async (req, res) => {
-		let newCourt = req.body;
-		return res.json({ create: newCourt });
-	});
+  .route("/create")
+  .get(async (req, res) => {
+    let isAuth;
+    if (req.session.user) {
+      isAuth = true;
+    } else {
+      isAuth = false;
+    }
+    return res.render("createCourt", { auth: isAuth, id: req.session.user.id });
+  })
+  .post(async (req, res) => {
+    let newCourt = req.body;
+    return res.json({ create: newCourt });
+  });
 
-router.route("/recommend/").get((req, res) => {
-	// return res.json({ route: "Recommended courts page" });
-	return res.render("recommendedCourts", {});
+router.route("/recommend").get((req, res) => {
+  // return res.json({ route: "Recommended courts page" });
+  return res.render("recommendedCourts", {
+    auth: true,
+    id: req.session.user.id,
+  });
 });
 
-router.route("/:courtId/").get(async (req, res) => {
-	let thisCourt = await getCourtById(req.params.courtId);
-	return res.render("courtById", { title: thisCourt.name, court: thisCourt, id: req.session.user.id });
+router.route("/:courtId").get(async (req, res) => {
+  let thisCourt = await getCourtById(req.params.courtId);
+  return res.render("courtById", {
+    title: thisCourt.name,
+    court: thisCourt,
+    id: req.session.user.id.toString(),
+  });
 });
 
 router.route("/:courtId/reserve").get(async (req, res) => {
-	let thisCourt;
-	try {
-		req.params.courtId = validId(req.params.courtId);
-		thisCourt = await getCourtById(req.params.courtId);
-	}
-	catch (e)
-	{
-		const strError = "This error occurred in the /:courtId/reserve route because a court with the supplied id does not exist. Click a link on the court by id page to avoid this issue.";
-		return res.status(404).json({error: strError});
-		//return res.status(404).render('error', {error: strError});
-	}
+  let thisCourt;
+  try {
+    let courtId = validId(req.params.courtId);
+    thisCourt = await getCourtById(courtId);
+  } catch (e) {
+    const strError =
+      "This error occurred in the /:courtId/reserve route because a court with the supplied id does not exist. Click a link on the court by id page to avoid this issue.";
+    return res.status(404).json({ error: strError });
+    //return res.status(404).render('error', {error: strError});
+  }
 
-	var currentDate = new Date();
-	var year = currentDate.getFullYear();
-	var month = String(currentDate.getMonth() + 1).padStart(2, "0");
-	var day = String(currentDate.getDate()).padStart(2, "0");
-	var currentDateStr = year + "-" + month + "-" + day;
+  var currentDate = new Date();
+  var year = currentDate.getFullYear();
+  var month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  var day = String(currentDate.getDate()).padStart(2, "0");
+  var currentDateStr = year + "-" + month + "-" + day;
 
-	var maxDate = new Date();
-	maxDate.setMonth(maxDate.getMonth() + 1);
-	var maxYear = maxDate.getFullYear();
-	var maxMonth = String(maxDate.getMonth() + 6).padStart(2, "0");
-	var maxDay = String(maxDate.getDate()).padStart(2, "0");
-	var maxDateStr = maxYear + "-" + maxMonth + "-" + maxDay;
+  var maxDate = new Date();
+  maxDate.setMonth(maxDate.getMonth() + 1);
+  var maxYear = maxDate.getFullYear();
+  var maxMonth = String(maxDate.getMonth() + 6).padStart(2, "0");
+  var maxDay = String(maxDate.getDate()).padStart(2, "0");
+  var maxDateStr = maxYear + "-" + maxMonth + "-" + maxDay;
 
-	return res.render("makeReservation", {
-		title: `Reserve ${thisCourt.name}`,
-		court: thisCourt,
-		id: thisCourt._id,
-		mindate: currentDateStr,
-		maxdate: maxDateStr,
-		schedule: thisCourt.schedule,
-	});
+  return res.render("makeReservation", {
+    title: `Reserve ${thisCourt.name}`,
+    court: thisCourt,
+    id: thisCourt._id,
+    mindate: currentDateStr,
+    maxdate: maxDateStr,
+    schedule: thisCourt.schedule,
+  });
 });
 
 router.route("/:courtId/reserve").post(async (req, res) => {
-	//TODO:userId validation
-	let thisCourt;
-	try {
-		//get court
-		req.params.courtId = validId(req.params.courtId);
-		thisCourt = await getCourtById(req.params.courtId);
-	}
-	catch (e)
-	{
-		const strError = "This error occurred in the /:courtId/reserve route because a court with the supplied id does not exist. Click a link on the court by id page to avoid this issue.";
-		return res.status(404).json({error: strError});
-		//return res.status(404).render('error', {error: strError});
-	}
-	try {
-		//validations
-		if (!req || !req.body)
-		{
-			const strError = "This error occurred because in the /:courtId/reserve route, it had no req body.";
-			return res.status(400).render('error', {error: strError});//number is good
-		}
+  //TODO:userId validation
+  let thisCourt;
+  try {
+    //get court
+    req.params.courtId = validId(req.params.courtId);
+    thisCourt = await getCourtById(req.params.courtId);
+  } catch (e) {
+    const strError =
+      "This error occurred in the /:courtId/reserve route because a court with the supplied id does not exist. Click a link on the court by id page to avoid this issue.";
+    return res.status(404).json({ error: strError });
+    //return res.status(404).render('error', {error: strError});
+  }
+  try {
+    //validations
+    if (!req || !req.body) {
+      const strError =
+        "This error occurred because in the /:courtId/reserve route, it had no req body.";
+      return res.status(400).render("error", { error: strError }); //number is good
+    }
 
-		//userId = validId(userId);
+    //userId = validId(userId);
 
-		let dateArr = req.body.selectedDate.split('-');
-		let newDateStr = dateArr[1] + "/" + dateArr[2] + "/" + dateArr[0];
-		newDateStr = validDate(newDateStr);
+    let dateArr = req.body.selectedDate.split("-");
+    let newDateStr = dateArr[1] + "/" + dateArr[2] + "/" + dateArr[0];
+    newDateStr = validDate(newDateStr);
 
-		req.body.startTime = validTime(req.body.startTime);
-		req.body.endTime = validTime(req.body.endTime);
+    req.body.startTime = validTime(req.body.startTime);
+    req.body.endTime = validTime(req.body.endTime);
 
-		validTimeInRange(
-			req.body.startTime,
-			req.body.endTime,
-			thisCourt.courtOpening,
-			thisCourt.courtClosing);
+    validTimeInRange(
+      req.body.startTime,
+      req.body.endTime,
+      thisCourt.courtOpening,
+      thisCourt.courtClosing
+    );
 
+    for (let i = 0; i < req.body.capacity.length; i++) {
+      if (
+        req.body.capacity.charCodeAt(i) < 48 ||
+        req.body.capacity.charCodeAt(i) > 57
+      ) {
+        throw `Error: capacity must contain all digits`;
+      }
+    }
+    let newCap = parseInt(req.body.capacity);
+    newCap = validNumber(newCap, "capacity", true, 0, thisCourt.capacity);
+  } catch (e) {
+    //const strError = "";
+    return res.status(404).json({ error: e });
+  }
 
-		for (let i=0;i<req.body.capacity.length;i++)
-		{
-			if (req.body.capacity.charCodeAt(i) < 48 || req.body.capacity.charCodeAt(i) > 57)
-			{
-				throw `Error: capacity must contain all digits`;
-			}
-		}
-		let newCap = parseInt(req.body.capacity);
-		newCap = validNumber(newCap, "capacity", true, 0, thisCourt.capacity);
-	}
-	catch (e)
-	{
-		//const strError = "";
-		return res.status(404).json({error: e});
-	}
+  try {
+    //data call
+    console.log("data call");
+    // let addedToSchedule = await addToSchedule(
+    // 	courtId,
+    // 	userId,//<-- issue b/c not yet validated
+    // 	newDateStr,
+    // 	req.body.startTime,
+    // 	req.body.endTime,
+    // 	newCap);
+  } catch (e) {
+    return res.status(404).json({ error: e });
+    //return res.status(404).render('error', {error: strError});
+  }
 
-	try {
-		//data call
-		console.log("data call")
-		// let addedToSchedule = await addToSchedule(
-		// 	courtId,
-		// 	userId,//<-- issue b/c not yet validated
-		// 	newDateStr,
-		// 	req.body.startTime,
-		// 	req.body.endTime,
-		// 	newCap);
-	}
-	catch (e)
-	{
-		return res.status(404).json({error: e});
-		//return res.status(404).render('error', {error: strError});
-	}
-
-	return res.render("makeReservation", {
-		title: `Reserve ${thisCourt.name}`,
-		court: thisCourt,
-		id: thisCourt._id,
-		mindate: currentDateStr,
-		maxdate: maxDateStr,
-		schedule: thisCourt.schedule,
-	});
+  return res.render("makeReservation", {
+    title: `Reserve ${thisCourt.name}`,
+    court: thisCourt,
+    id: thisCourt._id,
+    mindate: currentDateStr,
+    maxdate: maxDateStr,
+    schedule: thisCourt.schedule,
+  });
 });
 
 // router.route("/:courtId/cancel").get((req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,76 +1,84 @@
 import { Router } from "express";
 const router = Router();
 import {
-	createUser,
-	getUserById,
-	getUserByName,
-	getUserByUsername,
-	updateUser,
+  createUser,
+  getUserById,
+  getUserByName,
+  getUserByUsername,
+  updateUser,
 } from "../data/users.js";
 import { createReview } from "../data/reviews.js";
 import { getHistory } from "../data/history.js";
 import { getCourtById } from "../data/courts.js";
 
 router.route("/id/:userId").get(async (req, res) => {
-	try {
-    let isAuth;
-		if (req.session.user) {
-			isAuth = true;
-		}
-		else {
-			isAuth = false;
-		}
+  try {
+    // let isAuth;
+    // if (req.session.user) {
+    //   isAuth = true;
+    // } else {
+    //   isAuth = false;
+    // }
     let user = await getUserById(req.params.userId);
     res.render("profilePage", {
       id: req.session.user.id,
-			title: req.params.username,
-			user: user,
+      title: req.params.username,
+      user: user,
       reviews: user.reviews,
-      auth: isAuth
-		});
-	} catch (e) {
-		res.status(404).json({ error: "user not found" });
-	}
-	//return res.json({ userId: req.params.userId, implementMe: "<-" });
+      auth: true,
+    });
+  } catch (e) {
+    res.status(404).json({ error: "user not found" });
+  }
+  //return res.json({ userId: req.params.userId, implementMe: "<-" });
 });
 
 router
-	.route("/id/:userId/createReview")
+  .route("/id/:userId/createReview")
   .get(async (req, res) => {
-     let isAuth;
-		if (req.session.user) {
-			isAuth = true;
-		}
-		else {
-			isAuth = false;
-		}
-		let user = await getUserById(req.params.userId);
-		res.render("createReview", { title: req.params.username, user: user, id: req.session.user.id, auth: isAuth });
-	})
-	.post(async (req, res) => {
-		let reviewee = await getUserById(req.params.userId);
-		let reviewInfo = req.body;
+    let isAuth;
+    if (req.session.user) {
+      isAuth = true;
+    } else {
+      isAuth = false;
+    }
+    let user = await getUserById(req.params.userId);
+    res.render("createReview", {
+      title: req.params.username,
+      user: user,
+      id: req.session.user.id,
+      auth: isAuth,
+    });
+  })
+  .post(async (req, res) => {
+    let reviewee = await getUserById(req.params.userId);
+    let reviewInfo = req.body;
     let reviewer = await getUserById(req.session.user.id);
     reviewInfo["reviewer_id"] = req.session.user.id;
     reviewInfo["reviewerUsername"] = reviewer.username;
-		reviewInfo["revieweeUsername"] = reviewee.username;
+    reviewInfo["revieweeUsername"] = reviewee.username;
     reviewInfo["reviewee_id"] = reviewee._id;
     try {
-      let review = await createReview(reviewInfo.reviewee_id, reviewInfo.reviewer_id, Number(reviewInfo.rating), reviewInfo.comment);
+      let review = await createReview(
+        reviewInfo.reviewee_id,
+        reviewInfo.reviewer_id,
+        Number(reviewInfo.rating),
+        reviewInfo.comment
+      );
       if (review) {
         return res.redirect(`/user/id/${reviewInfo.reviewee_id}`);
       }
     } catch (e) {
-       return res.status(400).render('createReview', {bad: e});
+      return res.status(400).render("createReview", { bad: e });
     }
-		 /*try {
+    /*try {
       let review = await createReview(reviewInfo.reviewee_id, reviewInfo.reviewer_id, reviewInfo.rating, reviewInfo.comment);
        res.json({ create: reviewInfo });
     } catch (e) {
         return res.status(400).render('createReview', {bad: e});
     }
     res.status(500).render("createReview", { bad: "Internal Server Error" });*/
-	});
+  });
 
 /*router.route("/id/:userId/createReview").post(async(req, res) => {
   let newReview = await createReview(req.body);
@@ -78,25 +86,26 @@ router
 })*/
 
 router.route("/name/:username").get(async (req, res) => {
-	let username = await getUserByUsername(req.params.username);
-	return res.json({ username });
-	//return res.json({ username: req.params.username, implementMe: "<-" });
+  let username = await getUserByUsername(req.params.username);
+  return res.json({ username });
+  //return res.json({ username: req.params.username, implementMe: "<-" });
 });
 
 router.route("/id/:userId/history").get(async (req, res) => {
-	let courtHistory = await getHistory(req.params.userId);
-	console.log(courtHistory);
-	for (let i = 0; i < courtHistory.length; i++) {
-		let court = await getCourtById(courtHistory[i].court_id);
-		courtHistory[i].court_name = court.name;
-	}
-	// console.log(courtHistory)
-	// let link = `/user/id/${req.params.userId}/`;
-	return res.render("history", {
-		title: "History",
-		courts: courtHistory,
-		id: req.params.userId,
-	});
+  let courtHistory = await getHistory(req.params.userId);
+  console.log(courtHistory);
+  for (let i = 0; i < courtHistory.length; i++) {
+    let court = await getCourtById(courtHistory[i].court_id);
+    courtHistory[i].court_name = court.name;
+  }
+  // console.log(courtHistory)
+  // let link = `/user/id/${req.params.userId}/`;
+  return res.render("history", {
+    auth: true,
+    title: "History",
+    courts: courtHistory,
+    id: req.params.userId,
+  });
 });
 
 export default router;

56df5a65ea2e92bf299d4c827fee2db170964ed9
rgiova27 <rcgiovanniello@gmail.com>
2023-04-29 16:58:40 -0400

    reserve js start and handlebar changes

--- public/js/makeReservation.js
+++ public/js/makeReservation.js
@@ -1,10 +1,26 @@
+//import moment from 'moment';
 
+let makeReservationForm = document.getElementById('makeReservationForm');
 
-const displayAvailableTimes = () => {
-  //first clear old day's times
-  timesDiv.innerHTML = '';
+let selectedDate = document.getElementById('selectedDate');
+let startTime = document.getElementById('startTime');
+let endTime = document.getElementById('endTime');
+let capacity = document.getElementById('capacity');
 
-  const newDiv = document.createElement('div');
-  newDiv.innerHTML = 'HIII';
-  timesDiv.appendChild(newDiv);
+let errorDiv = document.getElementById('error');
+
+if (makeReservationForm) {
+  makeReservationForm.addEventListener('submit', (event) => {
+    try {
+      console.log('Reservation Form fired');
+      errorDiv.innerHTML = "";
+    }
+    catch (e)
+    {
+      console.log(e);
+      event.preventDefault();
+      //errorDiv.innerHTML = 'Invalid Input: ' + e;
+      errorDiv.hidden = false;
+    }
+  });
 }

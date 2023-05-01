1da71e4beb27a5616ae53c920a1b4771c9849dc4
jrose0116 <jrose0116@gmail.com>
2023-05-01 13:47:13 -0400

    Error example

--- public/js/makeReservation.js
+++ public/js/makeReservation.js
@@ -1,26 +1,26 @@
 //import moment from 'moment';
 
-let makeReservationForm = document.getElementById('makeReservationForm');
+let makeReservationForm = document.getElementById("makeReservationForm");
 
-let selectedDate = document.getElementById('selectedDate');
-let startTime = document.getElementById('startTime');
-let endTime = document.getElementById('endTime');
-let capacity = document.getElementById('capacity');
+let selectedDate = document.getElementById("selectedDate");
+let startTime = document.getElementById("startTime");
+let endTime = document.getElementById("endTime");
+let capacity = document.getElementById("capacity");
 
-let errorDiv = document.getElementById('error');
+let errorDiv = document.getElementById("error");
 
 if (makeReservationForm) {
-  makeReservationForm.addEventListener('submit', (event) => {
+  makeReservationForm.addEventListener("submit", (event) => {
     try {
-      console.log('Reservation Form fired');
+      console.log("Reservation Form fired");
       errorDiv.innerHTML = "";
-    }
-    catch (e)
-    {
+      if (capacity.value == 6) throw "6";
+    } catch (e) {
       console.log(e);
       event.preventDefault();
       //errorDiv.innerHTML = 'Invalid Input: ' + e;
       errorDiv.hidden = false;
+      errorDiv.innerHTML = e;
     }
   });
 }

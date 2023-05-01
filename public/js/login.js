22e75a776d884b00c6a4261ae14b7b45a618c923
jrose0116 <jrose0116@gmail.com>
2023-05-01 13:27:28 -0400

    Fix errorDiv

--- public/js/login.js
+++ public/js/login.js
@@ -1,64 +1,65 @@
-let loginForm = document.getElementById('login-form');
-let emailAddress = document.getElementById('emailAddressInput');
-let password = document.getElementById('passwordInput');
+let loginForm = document.getElementById("login-form");
+let emailAddress = document.getElementById("emailAddressInput");
+let password = document.getElementById("passwordInput");
+let errorDiv = document.getElementById("error-div");
 
 const validEmail = (email) => {
-    validStr(email);
-    email = email.trim();
-    let isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    if (!isValid) {
-      throw "Error: Invalid email address";
-    }
-    return email;
-}
+  validStr(email);
+  email = email.trim();
+  let isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  if (!isValid) {
+    throw "Error: Invalid email address";
+  }
+  return email;
+};
 
 if (loginForm) {
-    loginForm.addEventListener('submit', (event) => {
-        console.log("login submit js!!!!!!");
+  loginForm.addEventListener("submit", (event) => {
+    console.log("login submit js!!!!!!");
 
-        errorDiv.hidden = false;
-        errorDiv.innerHTML = "";
-        let emptyEmail = false;
-        let emptyPassword = false;
+    errorDiv.hidden = false;
+    errorDiv.innerHTML = "";
+    let emptyEmail = false;
+    let emptyPassword = false;
 
-        if (emailAddress.value.trim() === "") {
-            event.preventDefault();
-            emptyEmail = true;
-            let message = document.createElement('p');
-            message.innerHTML = "Email is required"
-            errorDiv.appendChild(message);
-        }
+    if (emailAddress.value.trim() === "") {
+      console.log("empty");
+      event.preventDefault();
+      emptyEmail = true;
+      let message = document.createElement("p");
+      message.innerHTML = "Email is required";
+      errorDiv.appendChild(message);
+    }
 
-        if (!emptyEmail) {
-            try {
-                emailAddress.value = validEmail(emailAddress.value);
-            }
-            catch (e) {
-                event.preventDefault();
-                let message = document.createElement('p');
-                message.innerHTML = "Email is not valid"
-                errorDiv.appendChild(message);        
-            }
-        }
+    if (!emptyEmail) {
+      try {
+        emailAddress.value = validEmail(emailAddress.value);
+      } catch (e) {
+        event.preventDefault();
+        let message = document.createElement("p");
+        message.innerHTML = "Email is not valid";
+        errorDiv.appendChild(message);
+      }
+    }
 
-        if (password.value.trim() === "") {
-            event.preventDefault();
-            emptyPassword = true;
-            let message = document.createElement('p');
-            message.innerHTML = "Password is required"
-            errorDiv.appendChild(message);
-        }
+    if (password.value.trim() === "") {
+      event.preventDefault();
+      emptyPassword = true;
+      let message = document.createElement("p");
+      message.innerHTML = "Password is required";
+      errorDiv.appendChild(message);
+    }
 
-        // if (!emptyPassword) {
-        //     try {
-        //         password.value = checkPassword(password.value);
-        //     }
-        //     catch (e) {
-        //         event.preventDefault();
-        //         let message = document.createElement('p');
-        //         message.innerHTML = "Password is not valid"
-        //         errorDiv.appendChild(message);        
-        //     }
-        // }
-    });
-  }
+    // if (!emptyPassword) {
+    //     try {
+    //         password.value = checkPassword(password.value);
+    //     }
+    //     catch (e) {
+    //         event.preventDefault();
+    //         let message = document.createElement('p');
+    //         message.innerHTML = "Password is not valid"
+    //         errorDiv.appendChild(message);
+    //     }
+    // }
+  });
+}
